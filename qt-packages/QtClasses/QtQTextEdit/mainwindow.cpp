b359d3edd5bafbac2b3473db80f54ca8b4aa83f1
santiagoom <ingenuity_zero@163.com>
2020-12-24 10:38:59 +0800

    QtQCheckBox, QtQTextEdit

---
+++ qt-packages/QtClasses/QtQTextEdit/mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+//ui->textBrowser->setText("hhh");
+ui->textBrowser->append("hhh");
+ui->textBrowser->append("hhh");
+ui->textBrowser->append("hhh");
+ui->textBrowser->append("hhh");
+ui->textEdit->setText("aa");
+ui->textEdit->append("hhh");
+ui->plainTextEdit->appendPlainText("ddd");
+// ui->plainTextEdit->verticalScrollBar()->setValue(ui->plainTextEdit->verticalScrollBar()->maximum());
+// ui->plainTextEdit->verticalScrollBar()->
+         ui->plainTextEdit->verticalScrollBar();
+}
